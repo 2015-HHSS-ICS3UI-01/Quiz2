438cec1783f974529d6caaa949899acd503ce036
duttr4019 <duttr4019@HRH-IC0027342.wrdsb.ca>
2015-09-29 12:47:29 -0400

    Finished quiz2

--- src/Quiz2.java
+++ src/Quiz2.java
@@ -4,14 +4,13 @@
  * and open the template in the editor.
  */
 
-
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
 
 /**
  *
- * @author lamon
+ * @author Ruchir
  */
 public class Quiz2 {
 
@@ -20,11 +19,18 @@ public class Quiz2 {
      */
     public static void main(String[] args) {
         // Can use Wall1.txt, Wall2.txt, or Wall3.txt
-        City kw = new City("Wall1.txt");
+        City kw = new City("Wall2.txt");
         Robot karel = new Robot(kw, 1, 1, Direction.WEST);
-        
-        //program solutions coded under here
-       
+
+        //Have karel check if front is not clear. 
+        while (!karel.frontIsClear()) {
+
+            //If front is not clear, have karel turn left and move on to the next wall.
+            karel.turnLeft();
+            karel.move();
+            karel.turnLeft();
+            karel.turnLeft();
+            karel.turnLeft();
+        }
     }
-    
 }
