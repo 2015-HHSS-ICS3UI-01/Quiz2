ba739ed2344c8f079a7b20466ac8a393499c4fb5
dawsr2694 <dawsr2694@HRH-IC0027330.wrdsb.ca>
2015-09-29 12:24:09 -0400

    Quiz2 completed

--- src/Quiz2.java
+++ src/Quiz2.java
@@ -4,14 +4,13 @@
  * and open the template in the editor.
  */
 
-
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
 
 /**
  *
- * @author lamon
+ * @author dawsonjoker
  */
 public class Quiz2 {
 
@@ -22,9 +21,16 @@ public class Quiz2 {
         // Can use Wall1.txt, Wall2.txt, or Wall3.txt
         City kw = new City("Wall1.txt");
         Robot karel = new Robot(kw, 1, 1, Direction.WEST);
-        
-        //program solutions coded under here
-       
+        while (true) {
+            if (!karel.frontIsClear()) {
+                karel.turnLeft();
+                karel.move();
+                karel.turnLeft(); //Right turn
+                karel.turnLeft();
+                karel.turnLeft();
+            } else if (karel.frontIsClear()) {
+                break;
+            }
+        }
     }
-    
 }
