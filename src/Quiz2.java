b91889f629c38fcf0f5f299f9eccb084779dab2e
howen2217 <howen2217@HRH-IC0027322.wrdsb.ca>
2016-03-29 08:23:46 -0400

    Completed the quiz.

--- src/Quiz2.java
+++ src/Quiz2.java
@@ -7,11 +7,11 @@
 
 import becker.robots.City;
 import becker.robots.Direction;
-import becker.robots.Robot;
+import becker.robots.RobotSE;
 
 /**
  *
- * @author lamon
+ * @author Nick Joudrey
  */
 public class Quiz2 {
 
@@ -21,10 +21,14 @@ public class Quiz2 {
     public static void main(String[] args) {
         // Can use Wall1.txt, Wall2.txt, or Wall3.txt
         City kw = new City("Wall1.txt");
-        Robot karel = new Robot(kw, 1, 1, Direction.WEST);
+        RobotSE karel = new RobotSE(kw, 1, 1, Direction.WEST);
         
         //program solutions coded under here
-       
+        while(!karel.frontIsClear()){
+            karel.turnLeft();
+            karel.move();
+            karel.turnRight();
+        }
     }
     
 }
